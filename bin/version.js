aa3056e12fcc5e9fbace5eccac58ecb7f63cba85
coderaiser <mnemonic.enemy@gmail.com>
2015-03-28 09:53:57 -0400

    feature(version) get version from package.json

--- bin/version.js
+++ bin/version.js
@@ -4,16 +4,27 @@
     'use strict';
     
     var version     = require('..'),
+        tryRequire  = require('try-require'),
         args        = process.argv.slice(2),
-        arg         = args[0];
+        arg         = args[0],
+        name        = process.cwd() + '/package.json',
+        info;
+    
+    if (!arg) {
+        info = tryRequire(name) || {};
         
-    if (arg === '-v' || arg === '--version')
+        if (!info)
+            console.error('package.json not found');
+        else
+            console.log(info.version || 'no version');
+    } else if (arg === '-v' || arg === '--version') {
         console.log('v' + require('../package').version);
-    else
+    } else {
         version(arg, function(error, data) {
             if (error)
                 console.error(error.message);
             else if (data)
                 console.log(data);
         });
+    }
 })();
