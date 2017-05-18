1f68309779cc23a953770cf3e4516083fd00c7b5
coderaiser <coderaiser@cloudcmd.io>
2017-05-18 11:27:04 +0300

    chore(version) rm iife

--- bin/version.js
+++ bin/version.js
@@ -1,38 +1,36 @@
 #!/usr/bin/env node
 
-(function() {
-    'use strict';
+'use strict';
+
+var version     = require('..'),
+    readjson    = require('readjson'),
+    tryCatch    = require('try-catch'),
+    args        = process.argv.slice(2),
+    arg         = args[0],
+    name        = process.cwd() + '/package.json',
     
-    var version     = require('..'),
-        readjson    = require('readjson'),
-        tryCatch    = require('try-catch'),
-        args        = process.argv.slice(2),
-        arg         = args[0],
-        name        = process.cwd() + '/package.json',
-        
-        error,
-        info;
+    error,
+    info;
+
+if (!arg) {
+    error = tryCatch(function() {
+        info = readjson.sync(name);
+    });
     
-    if (!arg) {
-        error = tryCatch(function() {
-            info = readjson.sync(name);
-        });
-        
-        if (error)
-            if (error.code === 'ENOENT')
-                console.error('package.json: not found');
-            else
-                console.error('package.json:', error.message);
+    if (error)
+        if (error.code === 'ENOENT')
+            console.error('package.json: not found');
         else
-            console.log(info.version || 'no version');
-    } else if (/^(-v|--version)$/.test(arg)) {
-        console.log('v' + require('../package').version);
-    } else {
-        version(arg, function(error, data) {
-            if (error)
-                console.error(error.message);
-            else if (data)
-                console.log(data);
-        });
-    }
-})();
+            console.error('package.json:', error.message);
+    else
+        console.log(info.version || 'no version');
+} else if (/^(-v|--version)$/.test(arg)) {
+    console.log('v' + require('../package').version);
+} else {
+    version(arg, function(error, data) {
+        if (error)
+            console.error(error.message);
+        else if (data)
+            console.log(data);
+    });
+}
